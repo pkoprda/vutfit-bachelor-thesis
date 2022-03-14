b2cbfb6ad123816d1521d9dc50a7c29573c7b4bc
pkoprda <petokoprda@gmail.com>
2022-03-14 23:57:36 +0100

    Added sliders for coordinates

--- app/main.py
+++ app/main.py
@@ -6,14 +6,14 @@ import folium
 from shapely.ops import unary_union
 from folium.plugins import HeatMap
 from itertools import chain
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 
 # FOR DEBUG: pd.options.display.max_colwidth = 300
 
 app = Flask(__name__, instance_relative_config=True)
 
-def create_map(latituge, longitude):
-    return folium.Map([latituge, longitude], zoom_start=17, max_zoom=19, width='75%', height='75%')
+def create_map(latituge, longitude, zoom_start=2):
+    return folium.Map([latituge, longitude], zoom_start=zoom_start, max_zoom=19, width='75%', height='75%')
 
 def get_geodataframe(north, south, east, west, tags):
     return leafmap.osm_gdf_from_bbox(north, south, east, west, tags)['geometry']
@@ -31,32 +31,32 @@ def get_coords(gdf):
             coords.append(list(zip(xcoords, ycoords)))
     return coords
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    north=49.2511
-    south=49.2382
-    east=16.5973
-    west=16.5636
     if request.method == 'POST':
-        north = request.form['north']
-        south = request.form['south']
-        east = request.form['east']
-        west = request.form['west']
-        return redirect('/')
-    else:
-        folium_map = create_map(49.242, 16.567)
+        north = float(request.form['north'])
+        south = float(request.form['south'])
+        east = float(request.form['east'])
+        west = float(request.form['west'])
+
+        center_lat = (north + south) / 2
+        center_long = (east + west) / 2
+        folium_map = create_map(center_lat, center_long, zoom_start=16)
         gdf = get_geodataframe(north, south, east, west, tags={"highway": True})
-        
         coords = list(chain(*get_coords(gdf)))
         longs, lats = list(zip(*coords))
-
         data = {"lats": lats, "longs": longs}
         df = pd.DataFrame(data)
 
         folium_map.add_child(HeatMap(df, name="Heat Map", min_opacity=0.2, radius=30, blur=50, max_zoom=1))
-        folium_map.save('app/templates/map.html')
-        return render_template('index.html')
+    else:
+        north, south, east, west = 49.2511, 49.2382, 16.5973, 16.5636
+        center_lat = (north + south) / 2
+        center_long = (east + west) / 2
+        folium_map = create_map(center_lat, center_long)
+    
+    folium_map.save('app/templates/map.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
