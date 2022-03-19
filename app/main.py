fbbe2cf1b1a2ebaac6cabe357ce2d461b33f1b39
pkoprda <petokoprda@gmail.com>
2022-03-19 13:32:00 +0100

    Added field with coordinates of mouse position

--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
-from urllib import request
 import pandas as pd
 import numpy as np
 from leafmap import osm_gdf_from_bbox
 from folium import Map as FoliumMap
 from shapely.ops import unary_union
-from folium.plugins import HeatMap
+from folium.plugins import HeatMap, MousePosition
 from itertools import chain
 from flask import Flask, render_template, request
 
@@ -12,11 +11,22 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, instance_relative_config=True)
 
+
 def create_map(latituge, longitude, zoom_start=2):
-    return FoliumMap([latituge, longitude], zoom_start=zoom_start, min_zoom=2, max_zoom=19, width='75%', height='75%')
+    m = FoliumMap(
+        [latituge, longitude], zoom_start=zoom_start, min_zoom=2,
+        max_zoom=19, width='75%', height='75%')
+    formatter = "function(num) {return L.Util.formatNum(num, 4) + ' º ';};"
+    MousePosition(
+        position='topright', separator=' | ', empty_string='',
+        lng_first=True, num_digits=20, prefix='Coordinates:',
+        lat_formatter=formatter, lng_formatter=formatter).add_to(m)
+    return m
+
 
 def get_geodataframe(north, south, east, west, tags):
     return osm_gdf_from_bbox(north, south, east, west, tags)['geometry']
+
 
 def get_coords(gdf):
     coords = []
@@ -27,14 +37,16 @@ def get_coords(gdf):
             distances = np.linspace(0, geom_obj.length, 30)
             points = [geom_obj.interpolate(distance) for distance in distances]
             multipoint = unary_union(points)
-            xcoords, ycoords = list(zip(*[(p.x, p.y) for p in multipoint]))        
+            xcoords, ycoords = list(zip(*[(p.x, p.y) for p in multipoint]))
             coords.append(list(zip(xcoords, ycoords)))
     return coords
+
 
 def valid_coords(first_coord, second_coord):
     if first_coord <= second_coord:
         return False
     return True
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -63,17 +75,21 @@ def index():
         except KeyError:
             app.logger.info("Could not create list of coordinates. Try different values...")
             return render_template('index.html', error_statement="Could not create heatmap")
+
         longs, lats = list(zip(*coords))
         data = {"lats": lats, "longs": longs}
         df = pd.DataFrame(data)
 
         app.logger.info("Creating a HeatMap...")
-        folium_map.add_child(HeatMap(df, name="Heat Map", min_opacity=0.2, radius=30, blur=50, max_zoom=1))
+        folium_map.add_child(HeatMap(
+            df, name="Heat Map", min_opacity=0.2,
+            radius=30, blur=50, max_zoom=1))
     else:
         folium_map = create_map(0.0, 0.0)
 
     folium_map.save('app/templates/map.html')
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
