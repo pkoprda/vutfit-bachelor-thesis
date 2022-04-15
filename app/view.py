0353d419290a16726785d97e9da1397dfdd394f5
pkoprda <petokoprda@gmail.com>
2022-04-15 02:48:50 +0200

    Allow default and custom coordinates

--- app/view.py
+++ app/view.py
@@ -15,15 +15,18 @@ def index():
             'east': round(float(request.form['east']), 5),
             'west': round(float(request.form['west']), 5)
         }
+        center_lat = (borders['north'] + borders['south']) / 2
+        center_long = (borders['east'] + borders['west']) / 2
+
         if invalid_coords(borders['north'], borders['south']):
-            return render_template('index.html', error_statement="North coordinate must be larger than south coordinate")
+            create_map(center_lat, center_long, zoom_start=15)
+            return render_template('index.html', borders=borders, error_statement="North coordinate must be larger than south coordinate")
         if invalid_coords(borders['east'], borders['west']):
-            return render_template('index.html', error_statement="East coordinate must be larger than west coordinate")
+            create_map(center_lat, center_long, zoom_start=15)
+            return render_template('index.html', border=borders, error_statement="East coordinate must be larger than west coordinate")
 
         if 'submit' in request.form:
             app.logger.info("Creating a Map...")
-            center_lat = (borders['north'] + borders['south']) / 2
-            center_long = (borders['east'] + borders['west']) / 2
             create_map(center_lat, center_long, zoom_start=15, create_heatmap=True)
             tags_highway = {"highway": True}
 
