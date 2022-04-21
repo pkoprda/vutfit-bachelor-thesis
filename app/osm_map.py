34f6980fe857758073cf05fbf73aee5c15db5e0c
pkoprda <petokoprda@gmail.com>
2022-04-21 23:51:17 +0200

    Added file with definitons of tags

--- app/osm_map.py
+++ app/osm_map.py
@@ -1,11 +1,16 @@
-import numpy as np
+from json import load as json_load
+from itertools import chain
+from numpy import linspace
+from pandas import DataFrame
 from shapely.ops import unary_union
-from leafmap import osm_gdf_from_bbox
+from osmnx.geometries import geometries_from_bbox
+from folium.plugins import HeatMap
+from app import app
 
 # FOR DEBUG: pd.options.display.max_colwidth = 300
 
 
-def create_map(center_lat, center_long, zoom_start=2, create_heatmap=False):
+def create_map(center_lat, center_long, zoom_start=2):
     with open('app/static/js/config_map.js', 'r') as f:
         data = f.readlines()
     data[1] = f"\tcenter: [{center_lat}, {center_long}], zoom: {zoom_start},\n"
@@ -15,22 +20,111 @@ def create_map(center_lat, center_long, zoom_start=2, create_heatmap=False):
 
 
 def get_geodataframe(north, south, east, west, tags):
-    return osm_gdf_from_bbox(north, south, east, west, tags)['geometry']
+    return geometries_from_bbox(north, south, east, west, tags)['geometry']
 
 
 def get_coords(gdf):
     coords = []
-    for geom_obj in gdf.loc[['way']].geometry:
-        if geom_obj.geom_type == 'Polygon':
-            coords.append(geom_obj.exterior.coords)
-        elif geom_obj.geom_type == 'LineString':
-            distances = np.linspace(0, geom_obj.length, 30)
+
+    # Polygon
+    if 'way' in gdf.index and gdf.loc[['way']].geometry.geom_type[0] == 'Polygon':
+        center_points = gdf.loc[['way']].geometry.centroid
+        xcoords, ycoords = list(zip(*[(p.x, p.y) for p in center_points]))
+        coords.append(list(zip(xcoords, ycoords)))
+        return coords
+
+    # LineString
+    if 'way' in gdf.index:
+        for geom_obj in gdf.loc[['way']].geometry:
+            distances = linspace(0, geom_obj.length, 30)
             points = [geom_obj.interpolate(distance) for distance in distances]
             multipoint = unary_union(points)
             xcoords, ycoords = list(zip(*[(p.x, p.y) for p in multipoint]))
             coords.append(list(zip(xcoords, ycoords)))
+        return coords
+
+    # Point
+    coords = [[(x, y) for x,y in zip(gdf.geometry.x , gdf.geometry.y)]]
     return coords
 
 
 def invalid_coords(first_coord, second_coord):
     return first_coord < second_coord
+
+
+def create_dataframe(gdf, error_message, key, value='all', probability=1.0):
+    try:
+        coords = list(chain(*gdf))
+    except:
+        error_message += f"{key}: {value}"
+        return DataFrame(), error_message
+
+    longs, lats = list(zip(*coords))
+    data = {'lats': lats, 'longs': longs, 'weight': probability}
+    return DataFrame(data), error_message
+
+
+def update_list_layers(df, heatmap_layers, gradient):
+    heatmap_data = HeatMap(df, name="HeatMap", min_opacity=0.2, radius=30, blur=50, max_zoom=1).data
+    heatmap_layers += f"""L.heatLayer({heatmap_data}, {{'blur': 50, 'gradient': {gradient}, 'maxZoom': 1, 'minOpacity': 0.2, 'radius': 30}}).addTo(folium_map)\n"""
+    return heatmap_layers
+
+
+def count_keys(data):
+    return sum([count_keys(v) if isinstance(v, dict) else 1 for v in data.values()])
+
+
+def create_heatlayers(borders, gradient):
+    with open('app/data/tags.json', 'r') as tags_file:
+        tags = json_load(tags_file)
+
+    error_message = ''
+    heatmap_layers = 'var heatmap = true;\n'
+    counter = 0
+    all = count_keys(tags)
+
+    for key in tags.keys():
+        # Exclude tags that are not in box
+        try:
+            if isinstance(tags[key], bool):
+                app.logger.info(f"{counter}/{all}: Creating a GeoDataFrame for '{key}'")
+            gdf = get_geodataframe(borders['north'], borders['south'], borders['east'], borders['west'], {key: True})
+        except:
+            pass
+
+        if gdf.empty:
+            counter += 1 if isinstance(tags[key], bool) else len(tags[key].keys())
+            app.logger.info(f"{counter}/{all}: No tag with key '{key}' found in this area")
+            continue
+
+        if isinstance(tags[key], bool):
+            gdf = get_coords(gdf)
+            df, error_message = create_dataframe(gdf, error_message, key)
+            if not df.empty:
+                app.logger.info(f"{counter}/{all}: Creating a Heatlayer for '{key}'")
+                heatmap_layers = update_list_layers(df, heatmap_layers, gradient)
+
+        else:
+            for value, probability in tags[key].items():
+                tag = {key: value}
+                counter += 1
+                try:
+                    app.logger.info(f"{counter}/{all}: Creating a GeoDataFrame for '{key}={value}'")
+                    gdf = get_geodataframe(borders['north'], borders['south'], borders['east'], borders['west'], tag)
+                except:
+                    app.logger.info(f"{counter}/{all}: Could not get GeoDataFrame for tag '{key}={value}'")
+                    error_message += f"{key}: {value}"
+                    continue
+
+                if gdf.empty:
+                    app.logger.info(f"{counter}/{all}: No tag with value '{key}={value}' found in this area")
+                    continue
+                gdf = get_coords(gdf)
+
+                df, error_message = create_dataframe(gdf, error_message, key, value, probability)
+                if not df.empty:
+                    app.logger.info(f"{counter}/{all}: Creating a Heatlayer for '{key}={value}'")
+                    heatmap_layers = update_list_layers(df, heatmap_layers, gradient)
+                    
+
+    return heatmap_layers, error_message
--- app/view.py
+++ app/view.py
@@ -1,9 +1,6 @@
-import pandas as pd
-from folium.plugins import HeatMap
-from itertools import chain
 from flask import render_template, request, jsonify
 from app import app
-from app.osm_map import create_map, get_geodataframe, get_coords, invalid_coords
+from app.osm_map import create_map, invalid_coords, create_heatlayers
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -26,40 +23,21 @@ def index():
             return render_template('index.html', border=borders, error_statement="East coordinate must be larger than west coordinate")
 
         if 'submit' in request.form:
-            tags_highway = {"highway": True}
-            try:
-                app.logger.info("Creating a GeoDataFrame of OSM entities within a N, S, E, W bounding box...")
-                gdf = get_geodataframe(borders['north'], borders['south'], borders['east'], borders['west'], tags_highway)
-            except:
-                app.logger.info("Could not get GeoDataFrame from that area. Try different values...")
-                create_map(center_lat, center_long, zoom_start=15, create_heatmap=False)
-                return render_template('index.html', borders=borders, error_statement="Could not create heatmap")
-
-            try:
-                app.logger.info("Creating list of coordinates...")
-                coords = list(chain(*get_coords(gdf)))
-            except KeyError:
-                app.logger.info("Could not create list of coordinates. Try different values...")
-                create_map(center_lat, center_long, zoom_start=15, create_heatmap=False)
-                return render_template('index.html', borders=borders, error_statement="Could not create heatmap")
-
             app.logger.info("Creating a Map...")
-            create_map(center_lat, center_long, zoom_start=15, create_heatmap=True)
+            create_map(center_lat, center_long, zoom_start=15)
 
-            longs, lats = list(zip(*coords))
-            data = {"lats": lats, "longs": longs}
-            df = pd.DataFrame(data)
-
-            app.logger.info("Creating a HeatMap...")
-            heatmap_list = HeatMap(df, name="HeatMap", min_opacity=0.2, radius=30, blur=50, max_zoom=1).data
             gradient = {"0.0": "#00008b", "0.05": "#0000a8", "0.1": "#0000c5", "0.15": "#0000e2", "0.2": "#0000ff", "0.25": "#003fff", "0.3": "#007fff", "0.35": "#00bfff", "0.4": "#00ffff", "0.45": "#3fffbf", "0.5": "#7fff7f", "0.55": "#bfff3f", "0.6": "#ffff00", "0.65": "#ffe900", "0.7": "#ffd200", "0.75": "#ffbc00", "0.8": "#ffa500", "0.85": "#ff7c00", "0.9": "#ff5200", "0.95": "#ff2900"}
-            heatmap_data = f"""var heatmap = L.heatLayer({heatmap_list}, {{'blur': 50, 'gradient': {gradient}, 'maxZoom': 1, 'minOpacity': 0.2, 'radius': 30}}).addTo(folium_map)\n"""
+            heatmap_layers, error_message = create_heatlayers(borders, gradient)
             with open('app/static/js/heatmap.js', 'w') as js_heatmap:
-                js_heatmap.write(heatmap_data)
+                js_heatmap.write(heatmap_layers)
+
+            if not len(error_message):
+                return render_template('index.html', borders=borders, error_statement=error_message)
+
             return render_template('index.html', borders=borders)
         else:
             return jsonify(borders=borders)
 
-    with open('app/static/js/heatmap.js', 'w') as js_heatmap: pass
+    with open('app/static/js/heatmap.js', 'w'): pass    
     create_map(0.0, 0.0)
     return render_template('index.html')
